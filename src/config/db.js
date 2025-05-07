55afc3a08159cb7e699829a18bcc6e6d3410cd7a
Tejas Giri <tejasgiri@ipgangram.com>
2025-05-07 12:10:01 +0530

    fix: update MongoDB connection URI to use the correct database name

--- src/config/db.js
+++ src/config/db.js
@@ -4,11 +4,13 @@ const logger = require('../utils/logger');
 const connectDB = async () => {
     //mongodb://localhost:27017/
   const { DB_URL } = process.env;
-  const uri = `${DB_URL}/interview_scheduler`;
+  const uri = `${DB_URL}`;
   try {
     await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      dbName: 'recruitment',
+      
     });
     logger.info('MongoDB connected');
   } catch (err) {
